562b7783c55cc0e1593c958c4dad9ec0b0822271
David Racovan <davidracovan@yahoo.com>
2025-01-04 23:45:38 -0500

    Add GDC auto

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -6,6 +6,7 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import com.techhounds.houndutil.houndauto.AutoRoutine;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Drivetrain;
 
 public class Autos {
@@ -13,5 +14,28 @@ public class Autos {
         PathPlannerPath path = PathPlannerPath.fromPathFile("testPath");
         Command command = drivetrain.followPathCommand(path);
         return new AutoRoutine("testPath", command, List.of(path), path.getStartingDifferentialPose());
+    }
+
+    public static AutoRoutine GDC(Drivetrain drivetrain) {
+        PathPlannerPath Start_GPath = PathPlannerPath.fromPathFile("Start-G");
+        PathPlannerPath G_PickupPath = PathPlannerPath.fromPathFile("G-Pickup");
+        PathPlannerPath Pickup_DPath = PathPlannerPath.fromPathFile("Pickup-D");
+        PathPlannerPath D_PickupPath = PathPlannerPath.fromPathFile("D-Pickup");
+        PathPlannerPath Pickup_CPath = PathPlannerPath.fromPathFile("Pickup-C");
+
+        Command command = Commands.sequence(
+                drivetrain.followPathCommand(Start_GPath),
+                Commands.waitSeconds(1),
+                drivetrain.followPathCommand(G_PickupPath),
+                Commands.waitSeconds(1),
+                drivetrain.followPathCommand(Pickup_DPath),
+                Commands.waitSeconds(1),
+                drivetrain.followPathCommand(D_PickupPath),
+                Commands.waitSeconds(1),
+                drivetrain.followPathCommand(Pickup_CPath));
+
+        return new AutoRoutine("GDC", command,
+                List.of(Start_GPath, G_PickupPath, Pickup_DPath, D_PickupPath, Pickup_CPath),
+                Start_GPath.getStartingDifferentialPose());
     }
 }
