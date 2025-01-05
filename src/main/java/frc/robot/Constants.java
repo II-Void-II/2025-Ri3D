2cb2ef216c4524413fdd3a2f252f3b4bf56503f2
David Racovan <davidracovan@yahoo.com>
2025-01-04 23:45:53 -0500

    Set feedforwards from SysId

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,18 +10,16 @@ public class Constants {
 
     public static final class Drivetrain {
 
-        public static final int LEFT_PRIMARY_MOTOR_ID = 1;
-        public static final int LEFT_SECONDARY_MOTOR_ID = 2;
-        public static final int RIGHT_PRIMARY_MOTOR_ID = 3;
-        public static final int RIGHT_SECONDARY_MOTOR_ID = 4;
+        public static final int LEFT_PRIMARY_MOTOR_ID = 3;
+        public static final int LEFT_SECONDARY_MOTOR_ID = 4;
+        public static final int RIGHT_PRIMARY_MOTOR_ID = 1;
+        public static final int RIGHT_SECONDARY_MOTOR_ID = 2;
 
         public static final boolean LEFT_DRIVE_MOTORS_INVERTED = false; // TODO
         public static final boolean RIGHT_DRIVE_MOTORS_INVERTED = true; // TODO
 
         /** Distance between left and right wheels. */
-        public static final double TRACK_WIDTH_METERS = 0.527; // TODO
-        /** Distance between front and back wheels. */
-        public static final double WHEEL_BASE_METERS = 0.527; // TODO
+        public static final double TRACK_WIDTH_METERS = Units.inchesToMeters(22); // TODO
         public static final double DRIVE_BASE_RADIUS_METERS = 0.3727; // TODO
 
         public static final double GEARING = 8.45;
@@ -37,11 +35,11 @@ public class Constants {
         public static final double VELOCITY_kP = 1.5;
         public static final double VELOCITY_kI = 0.0;
         public static final double VELOCITY_kD = 0.0;
-        public static final double kS = 0.0519492;
-        public static final double kV_LINEAR = 2.1969;
-        public static final double kA_LINEAR = 0.252588;
-        public static final double kV_ANGULAR = 0;
-        public static final double kA_ANGULAR = 0;
+        public static final double kS = 0.29971;
+        public static final double kV_LINEAR = 2.4;
+        public static final double kA_LINEAR = 0.56555;
+        public static final double kV_ANGULAR = 1.2427;
+        public static final double kA_ANGULAR = 0.0859;
 
         public static final double POSITION_kP = 1.5;
         public static final double POSITION_kI = 0.0;
@@ -70,6 +68,30 @@ public class Constants {
 
         public static final DifferentialDriveKinematics KINEMATICS = new DifferentialDriveKinematics(
                 TRACK_WIDTH_METERS);
+    }
+
+    public static final class Elevator {
+        public static enum ElevatorPosition {
+            BOTTOM(0);
+
+            public final double value;
+
+            private ElevatorPosition(double value) {
+                this.value = value;
+            }
+        }
+    }
+
+    public static final class Arm {
+        public static enum ArmPosition {
+            BOTTOM(0);
+
+            public final double value;
+
+            private ArmPosition(double value) {
+                this.value = value;
+            }
+        }
     }
 
     public static final class Controls {
